a7c38bbadc7f4e713ea775d12529d617a1460f3a
LemoZun <marrzun@gmail.com>
2024-08-21 19:40:16 +0900

    feat : 짝수 홀수 개수 배열 반환

---
+++ Q120824/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Q120824
+{
+    public class Program
+    {
+        /// <summary>
+        /// 짝수 홀수 개수 Q120824
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/120824
+        /// </summary>
+        public class Solution
+        {
+
+            public int[] solution(int[] num_list)
+            {
+                int[] answer = new int[2];
+                foreach (int i in num_list)
+                {
+                    if (i % 2 == 0) answer[0]++;
+                    else answer[1]++;
+                }
+                
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
