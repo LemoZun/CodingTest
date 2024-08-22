7360489db4e901cfd9ba38308e224d3dd10cc48b
LemoZun <marrzun@gmail.com>
2024-08-22 19:24:40 +0900

    feat : 양꼬치

---
+++ Q120830/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Q120830
+{
+    public class Program
+    {
+        /// <summary>
+        /// 양꼬치 Q120830
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/120830
+        /// </summary>
+        public class Solution
+        {
+            public int solution(int n, int k)
+            {
+                int answer = 12000 * n + 2000 * (k - n / 10);
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
