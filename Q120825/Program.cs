31c978a56f5afec2c7645f5c97494ec92c1c9b89
LemoZun <marrzun@gmail.com>
2024-08-22 18:54:04 +0900

    feat : 문자 반복 출력하기

---
+++ Q120825/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace Q120825
+{
+    public class Program
+    {
+        /// <summary>
+        /// 문자 반복 출력하기 Q120825
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/120825
+        /// </summary>
+        public class Solution
+        {
+            public string solution(string my_string, int n)
+            {
+                string answer = "";
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < my_string.Length; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        sb.Append(my_string[i]);
+                    }
+                }
+                answer = sb.ToString();
+
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            
+            Console.WriteLine(s.solution("hello", 3));
+        }
+    }
+}
